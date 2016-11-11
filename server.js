c3ec0a2da5e08336c93fe543bdff2e1a4da22b08
Mounya246 <mounya2u@gmail.com>
2016-11-11 20:04:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    host: 'http://db.imad.hasura-app.io/',
+  user: 'mounya246',
+  port:'5432',
+  password: process.env.DB_PASSWORD,
+  database: 'mounya246',
+};
 var app = express();
 app.use(morgan('combined'));
+
 
 
 var articles=
@@ -84,7 +92,21 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool = new Pool(config);
 
+app.get('/test.db',function(req,res)
+{
+    pool.query('SELECT*FROM test',function(err,result)
+    {
+        if(err)
+        {res.status(500).send(err.toString());
+        }else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
+    
+});
 var counter=0;
 app.get('/counter',function (req,res)
 {
