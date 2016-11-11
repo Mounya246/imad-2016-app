97ce38869aa9df73eff761c010348f85b404532e
Mounya246 <mounya2u@gmail.com>
2016-11-11 18:06:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,18 +5,117 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles=
+{
+        'article-one':{
+            title:'Article One|Arunima Sharma',
+            heading:'Article One',
+            date:'10th October 2016',
+            content:`<p>
+                        This is the content for my first article..
+                    </p>
+                    <p>
+                        I am currently working in imad console.
+                    </p>`
+},
+'article-two':{
+    title:'Article Two|Arunima Sharma',
+            heading:'Article Two',
+            date:'10th October 2016',
+            content:`<p>
+                        This is the content for my second article..
+                    </p>
+                    <p>
+                        I am currently working in imad console.
+                    </p>`
+},
+'article-three':{
+    title:'Article Three|Arunima Sharma',
+            heading:'Article Three',
+            date:'10th October 2016',
+            content:`<p>
+                        This is the content for my third article..
+                    </p>
+                    <p>
+                        I am currently working in imad console.
+                    </p>`
+}
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+
+var htmlTemplate=`
+<html>
+<head>
+    <title>
+        ${title}
+    </title>
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+    </div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
+    
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter=0;
+app.get('/counter',function (req,res)
+{
+    counter=counter+1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var names=[];
+app.get('/submit-name',function(req,res){
+   var name=req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
